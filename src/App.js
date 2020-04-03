1cbad33c1b35e65c9232c2e56b83bdefac2c921e
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2020-04-03 13:30:17 -0700

    Implemented Sign Up component for new users

--- src/App.js
+++ src/App.js
@@ -19,9 +19,21 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      CreateUserProfileDocument(user)
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await CreateUserProfileDocument(userAuth)
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      this.setState({currentUser: userAuth})
     })
   }
 
