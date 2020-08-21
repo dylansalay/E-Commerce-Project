ecb22d5522b7864c04c40f1d5606b48dd13c892e
dylan salay <dylansalay@gmail.com>
2020-08-21 08:11:13 -0700

    Fix cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom'
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import {toggleCartHidden } from '../../redux/cart/cart.actions.js'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -22,7 +23,12 @@ const CartDropdown = ({ cartItems, history }) => (
         )
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>Go To Checkout</CustomButton>
+    <CustomButton onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden());
+    }}>
+      Go To Checkout
+    </CustomButton>
   </div>
 )
  const mapStateToProps = createStructuredSelector ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,23 +1,33 @@
-import { createSelector } from 'reselect'
+import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
-)
+);
 
 export const selectCartHidden = createSelector(
   [selectCart],
-  cart => cart.cartHidden
-)
+  cart => cart.hidden
+);
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  cartItems => cartItems.reduce((accumulatedQuantity, cartItems) => accumulatedQuantity + cartItems.quantity, 0)
-)
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
 
 export const selectCartTotal = createSelector(
   [selectCartItems],
-  cartItems => cartItems.reduce((accumulatedQuantity, cartItems) => accumulatedQuantity + cartItems.quantity * cartItems.price, 0)
-)
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+      0
+    )
+);
